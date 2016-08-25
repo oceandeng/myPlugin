53881fc46b77b1c93b80c38b6875095a95734a42
ocean_deng <ocean_deng@163.com>
2016-08-25 20:19:25 +0800

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,7 +7,7 @@ import {stream as wiredep} from 'wiredep';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
-const app = "echart";
+const app = "app";
 
 gulp.task('styles', () => {
   return gulp.src(app + '/styles/*.scss')
